d7ea5a570163681aa1cb2178ac11ad62652737a6
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2024-09-22 15:53:01 -0300

    spotless plugin

--- src/main/java/permission_manager/permission/PermissionApplication.java
+++ src/main/java/permission_manager/permission/PermissionApplication.java
@@ -2,14 +2,11 @@ package permission_manager.permission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PermissionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PermissionApplication.class, args);
-	}
-
-
+  public static void main(String[] args) {
+    SpringApplication.run(PermissionApplication.class, args);
+  }
 }
